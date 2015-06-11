4b164414894d5ffc1c5667a781bc3b20caca15d5
SirNanoCat <SirNanoCat@gmail.com>
2015-06-11 22:20:39 +0100

    BasicSpriteDrawing example game
    added sample and project files. content still needs to be added

---
+++ BasicSpriteDrawing/Game1.cs
@@ -0,0 +1,95 @@
+﻿#region Using Statements
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
+using Nano.Engine.Sprites;
+#endregion
+
+namespace BasicSpriteDrawing
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager m_Graphics;
+        SpriteBatch m_SpriteBatch;
+
+        ISprite m_Sprite;
+
+        public Game1()
+        {
+            m_Graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";	            
+            m_Graphics.IsFullScreen = true;		
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+				
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            m_SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //TODO: use this.Content to load your game content here
+            Texture2D texture = Content.Load<Texture2D>("ship");
+            m_Sprite = new BasicSprite(texture);
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if(Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+            // TODO: Add your update logic here			
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            m_Graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+		
+            //TODO: Add your drawing code here
+            int x = m_Graphics.GraphicsDevice.Viewport.Width / 2;
+            int y = m_Graphics.GraphicsDevice.Viewport.Height / 2;
+
+            m_Sprite.Position = new Vector2(x,y);
+
+            m_SpriteBatch.Begin();
+            m_Sprite.Draw(m_SpriteBatch);
+            m_SpriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
+
