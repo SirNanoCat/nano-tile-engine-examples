c5adbb853045fb020239c13e4c6fbd88d55df148
SirNanoCat <SirNanoCat@gmail.com>
2015-06-26 07:16:47 +0100

    Updated Tileset example

--- Tileset/TilesetGame.cs
+++ Tileset/TilesetGame.cs
@@ -7,6 +7,10 @@ using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
 using Nano.Input;
 using Nano.Engine.Sys;
+using Nano.Engine.Graphics;
+using Nano.Engine.Graphics.Tileset;
+using System.Collections.Generic;
+using Nano.Engine.Graphics.Sprites;
 
 #endregion
 
@@ -17,15 +21,20 @@ namespace Tileset
     /// </summary>
     public class TilesetGame : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        GraphicsDeviceManager m_Graphics;
         IInputService m_Input;
+        ISpriteManager m_SpriteManager;
+        ITileset m_Tileset;
+        List<ISprite> m_Sprites;
+        Random m_RNG;
 
         public TilesetGame()
         {
-            graphics = new GraphicsDeviceManager(this);
+            m_Graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";	            
-            graphics.IsFullScreen = true;		
+            m_Graphics.IsFullScreen = true;
+            m_Sprites = new List<ISprite>();
+            m_RNG = new Random(123);
         }
 
         /// <summary>
@@ -37,12 +46,14 @@ namespace Tileset
         protected override void Initialize()
         {
             base.Initialize();
+
+            ServiceLocator.Instance.SetServiceContainer(Services);
 		
             var input = new InputManager(this);
             Components.Add(input);
             m_Input = input;
-            ServiceLocator.Services.AddService<IInputService>(m_Input);
 
+            ServiceLocator.Services.AddService<IInputService>(m_Input);
         }
 
         /// <summary>
@@ -51,8 +62,24 @@ namespace Tileset
         /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            m_SpriteManager = new SpriteManager(Content, new SpriteBatch(m_Graphics.GraphicsDevice));
+            var tex2D = m_SpriteManager.CreateTexture2D("spritesheets/test-sprite-sheet");
+            m_Tileset = new RegularTileset("test-tileset", tex2D, 4, 4, 32, 32);
+
+            int screenWidth = m_Graphics.GraphicsDevice.Viewport.Width;
+            int screenHeight = m_Graphics.GraphicsDevice.Viewport.Height;
+           
+            foreach(var bounds in m_Tileset.Bounds)
+            {
+                ISprite sprite = m_SpriteManager.CreateSprite(tex2D, bounds);
+
+                int x = m_RNG.Next(0,screenWidth);
+                int y = m_RNG.Next(0, screenHeight);
+
+                sprite.Position = new Vector2(x, y);
+
+                m_Sprites.Add(sprite);
+            }
         }
 
         /// <summary>
@@ -62,10 +89,26 @@ namespace Tileset
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            base.Update(gameTime);
+
             if (m_Input.KeyDown(Keys.Escape))
                 Exit();
 
-            base.Update(gameTime);
+            //randomise sprite positions when space is pressed
+            if( m_Input.KeyPressed(Keys.Space))
+            {
+                int screenWidth = m_Graphics.GraphicsDevice.Viewport.Width;
+                int screenHeight = m_Graphics.GraphicsDevice.Viewport.Height;
+
+                foreach(var sprite in m_Sprites)
+                {
+                    int x = m_RNG.Next(0,screenWidth);
+                    int y = m_RNG.Next(0, screenHeight);
+
+                    sprite.Position = new Vector2(x, y);
+                }
+            }
+
         }
 
         /// <summary>
@@ -74,9 +117,12 @@ namespace Tileset
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+            m_Graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
 		
-            //TODO: Add your drawing code here
+            foreach(var sprite in m_Sprites)
+            {
+                sprite.Draw();
+            }
             
             base.Draw(gameTime);
         }
